e5e713eef0e9a37674fd32fe60146190252b995b
NathanDSanta <natan.d.santa@gmail.com>
2024-11-06 10:55:10 +0100

    just started

---
+++ treball/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+const int N_MIDES = 6;
+const int MIDES[N_MIDES] = {500, 5000, 10000, 50000, 100000, 1000000} 
+
+
+
+int main(){
+  taules[N_MIDES][MIDES[N_MIDES]];  
+
+  return 0;
+}
